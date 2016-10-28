0b95e611aa91fd91d8a477fe6fee313987ef1ed9
Pasquale Boemio <pasquale.boemio@statwolf.com>
2016-10-28 15:19:56 +0100

    first commit

---
+++ spec/statwolf-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StatwolfView from '../lib/statwolf-view';
+
+describe('StatwolfView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
